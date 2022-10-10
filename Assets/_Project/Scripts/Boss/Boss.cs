6405f488b7e3178c902d4731ac3026fc486dd4b2
Rexmxxrayder <57299654+Rexmxxrayder@users.noreply.github.com>
2022-10-10 23:09:50 +0200

    boss begins

---
+++ Assets/_Project/Scripts/Boss/Boss.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+    List<EntityThreatPoint> threatPoints;
+
+    void Attack(Transform target) {
+
+    }
+}

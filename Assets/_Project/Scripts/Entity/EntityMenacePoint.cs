9df537eaaa0c4b036123083d5a3d02aa388c729b
Wizerdy <55747980+Wizerdy@users.noreply.github.com>
2022-10-13 20:02:11 +0200

    [ADD] Menace Points
    Comment faire un bon menace point system :
    - Tu mets ton Menace Point System sur un objet plus ou moins vide (Comme tu veux).
    - Tu crées une référence pour Menace Point System et tu attaches un reference setter au Menace Point System précedemment créer.
    - Tu mets sur ton player (de préférence dans Abilities) le script EntityMenacePoint et tu mets les références bien comme il faut.
    - Vous avez un système fonctionnel ! Amusez-vous bien avec les menaces !

---
+++ Assets/_Project/Scripts/Entity/EntityMenacePoint.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using ToolsBoxEngine;
+
+public class EntityMenacePoint : MonoBehaviour, IEntityAbility {
+    [SerializeField] MenacePointSystemReference _menacePointSystem;
+    [SerializeField] EntityAbilities _entityAbilities;
+
+    public int Menace => _menacePointSystem?.Instance?.Get(_entityAbilities) ?? -1;
+
+    private void Start() {
+        if (_menacePointSystem.IsValid()) {
+            _menacePointSystem.Instance.Add(_entityAbilities);
+        }
+    }
+
+    private void OnDestroy() {
+        if (_menacePointSystem.IsValid()) {
+            _menacePointSystem.Instance.Remove(_entityAbilities);
+        }
+    }
+
+    public void Add(int count) {
+        if (!_menacePointSystem.IsValid()) { return; }
+        _menacePointSystem.Instance.Add(_entityAbilities, count);
+    }
+
+    public void Remove(int count) {
+        if (!_menacePointSystem.IsValid()) { return; }
+        _menacePointSystem.Instance.Remove(_entityAbilities, count);
+    }
+}

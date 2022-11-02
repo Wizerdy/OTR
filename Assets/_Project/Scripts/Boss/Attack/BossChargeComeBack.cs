c1f9a1a1807363c2b5a54a58986a36129c091a4e
Rexmxxrayder <57299654+Rexmxxrayder@users.noreply.github.com>
2022-11-02 12:42:15 +0100

    des boules

--- Assets/_Project/Scripts/Boss/Attack/BossChargeComeBack.cs
+++ Assets/_Project/Scripts/Boss/Attack/BossChargeComeBack.cs
@@ -5,7 +5,7 @@ using static UnityEngine.GraphicsBuffer;
 
 public class BossChargeComeBack : BossCharge {
     [SerializeField] protected float _speedComeback;
-    protected override IEnumerator Attack(EntityAbilities ea, Transform target) {
+    protected override IEnumerator AttackMiddle(EntityAbilities ea, Transform target) {
         yield return StartCoroutine(Charge(ea.transform.position, target.position, _delayBeforeCharge, _speed));
         yield return StartCoroutine(ChargeDestination(ea.transform, Vector3.zero, 0f, _speedComeback));
     }
--- Assets/_Project/Scripts/Boss/Attack/BossChargeTeleport.cs
+++ Assets/_Project/Scripts/Boss/Attack/BossChargeTeleport.cs
@@ -6,7 +6,7 @@ using ToolsBoxEngine;
 public class BossChargeTeleport : BossCharge {
     [SerializeField] float _delayBeforeTeleport;
     [SerializeField] float _distFromPlayer;
-    protected override IEnumerator Attack(EntityAbilities ea, Transform target) {
+    protected override IEnumerator AttackMiddle(EntityAbilities ea, Transform target) {
         yield return new WaitForSeconds(_delayBeforeTeleport);
         ea.transform.position = target.position + (Vector3.zero - target.position).normalized * _distFromPlayer;
         yield return StartCoroutine(Charge(ea.transform.position, target.position, _delayBeforeCharge, _speed));

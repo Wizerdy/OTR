96a73018e4320fb8fc1a53b17da63fbb299d30a9
Darnald0 <dudavid1@hotmail.fr>
2022-12-12 01:34:40 +0100

    stop particle

---
+++ Assets/_Project/Scripts/PlayVFX.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVFX : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem bloodDashParticle;
+
+    public void PlayBloodDashParticle() {
+        bloodDashParticle.Play();
+    }
+}
